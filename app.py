11fd31bace43a67873cb59a5729dc46d01b1b01e
Jérôme Buisine <jerome.buisine@outlook.fr>
2022-09-08 12:15:58 +0200

    Deploy webhook command

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
 """
-Flask application routes
+Flask webhook application 
 """
 import os
 import json
+import hmac
+import hashlib
 
-from flask import Flask
+from flask import Flask, request
 
 # get configuration
-config = None
 with open("config.json") as f:
     config = json.load(f)
 
 # initialize the flask app
 app = Flask("webhook_app", static_folder='./web/static', template_folder='./web/templates')
-app.secret_key = b''
+app.secret_key = bytes(f"{config['flask_secret']}", 'utf-8')
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     """
     Route which tracks webhook
     """
-    return json.dumps(config)
+    
+    if verify_signature(request, config['webhook_secret']):
+        
+        print("Secret has been verified")
+        print(f"Running custom command... {config['command']}")
+        os.system(config['command'])
+        return json.dumps({"message": "ok"})
+    else:
+        print("Unexpected secret")
+        return json.dumps({"message": "error"})
+
+def verify_signature(req, secret):
+    """Compare secret signature
+
+    Returns:
+        {bool} -- if signature is ok or not
+    """
+    received_sign = req.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
+    secret = secret.encode()
+    expected_sign = hmac.HMAC(key=secret, msg=req.data, digestmod=hashlib.sha256).hexdigest()
+    return hmac.compare_digest(received_sign, expected_sign)
